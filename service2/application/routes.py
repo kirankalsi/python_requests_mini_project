16a5b71818df3b1a231571628baa854964cf3c73
kirankalsi <kiran.1998@hotmail.co.uk>
2020-11-23 16:07:55 +0000

    structure

---
+++ service2/application/routes.py
@@ -0,0 +1,4 @@
+
+
+animals = ['Dog', 'Cat', 'Lion', 'Cow', 'Sheep', 'Donkey', 'Duck']
+sounds = {'Dog':'Woof', 'Cat':'Meow', 'Lion':'Roar', 'Cow':'Moo', 'Sheep':'Baa', 'Donkey':'Hee-Haw', 'Duck':'Quack'}
