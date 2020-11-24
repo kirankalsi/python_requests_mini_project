04b5b4ef4de2d3997669150e53980cfcdef40e28
kirankalsi <kiran.1998@hotmail.co.uk>
2020-11-24 19:37:46 +0000

    connectioon refused

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -1,12 +1,14 @@
-from flask import render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request
 import requests
 
 from application import app
 
+#app = Flask(__name__)
+
 @app.route('/', methods=['GET'])
 def index():
-    animal = requests.get("http://localhost:5001/animal")
-    sound = requests.post("http://localhost:5001/sound", data=animal.text)
+    animal = requests.get("http://34.105.145.24:5001/animal")
+    sound = requests.post("http://34.105.145.24:5001/sound", data=animal.text)
     return render_template('index.html', animal=animal.text, sound=sound.text)
     #sound = requests.post("http://localhost:5000/sound", data=animal.json)
     #return render_template('index.html', animal=animal.json, sound=sound.json)
