7a0aa31219187a2cbf6490d9b3e381263292e51c
kirankalsi <kiran.1998@hotmail.co.uk>
2020-11-26 12:54:24 +0000

    update scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,3 @@
 #! /bin/bash
+
+docker stack deploy --compose-file docker-compose.yaml my-stack
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,1 +1,4 @@
 #! /bin/bash
+
+pytest ./service1 --cov ./service1/application
+pytest ./service2 --cov ./service2/application
