c47e33e9433a2f86e96c96ce6ee295dfc328dff4
kirankalsi <kiran.1998@hotmail.co.uk>
2020-11-26 14:49:38 +0000

    test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
 
+sudo apt update
+sudo apt install python3 python3-pip python3-venv
+python3 -m venv proj_venv
+. proj_venv/bin/activate
+pip3 install -r service1/requirements.txt
+
 pytest ./service1 --cov ./service1/application
 pytest ./service2 --cov ./service2/application
+
+deactivate
+rm -rf proj_venv
